a114f783561118d43bc96b0efa4cfe178f15fe3e
T Smith <87288866+T-Smith45@users.noreply.github.com>
2025-10-18 21:23:43 -0500

    move to dotnet 9
    dotnet 10 does not work with azure functions

---
+++ az-func.Demo/az-func.demo2/FastApi.cs
@@ -0,0 +1,34 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace az_func.demo2;
+
+public class FastApi
+{
+    private readonly ILogger<FastApi> _logger;
+    private readonly IQuoteService _quoteService;
+
+    public FastApi(ILogger<FastApi> logger , IQuoteService quoteService)
+    {
+        _logger = logger;
+        _quoteService = quoteService;
+    }
+
+    [Function("DailyQuotes")]
+    public IActionResult GetDailyQuotes([HttpTrigger(AuthorizationLevel.Anonymous,"get", Route = "daily-quote")] HttpRequest req, string? category)
+    {
+        category ??= "all";
+        _logger.LogInformation("Incoming Category: {category}", category);
+        var q = category != "all" 
+            ? _quoteService.GetRandomQuoteWithCategory(category)
+            : _quoteService.GetRandomQuote();
+
+        var time = DateTime.Now.ToUniversalTime().ToString("yyyy-MM-dd HH:mm:ss");
+        var resp = new OkObjectResult(new { quote = q , generateTime = time});
+        _logger.LogInformation("Generated Response: Quote: {Author}, Time: {Time}", q.author, time);
+        return resp;
+    }
+
+}
